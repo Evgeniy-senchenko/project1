78b2dbb57eb3448b713dc7e64ed726dac7bfb55e
kirdmya <kirdmya@gmail.com>
2023-01-19 20:11:03 +0300

    Create

---
+++ func.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int func1(){
+    printf("This is func1\n");
+    return 0;
+}
+
+int func2(){
+    printf("This is func2()\n");
+    return 0;
+}
+
+int main(){
+    printf("Hello, World!\n");
+    printf("You're welcome!\n");
+    return 0;
+}
